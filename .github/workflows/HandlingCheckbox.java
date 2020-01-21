9c89db03dfed6006a22a3357332211980bacf2a0
Gagan Sharma <41976317+gagansharma1999@users.noreply.github.com>
2020-01-21 15:54:23 +0530

    Update HandlingCheckbox.java

--- .github/workflows/HandlingCheckbox.java
+++ .github/workflows/HandlingCheckbox.java
@@ -16,6 +16,12 @@ public static void main(String[] args)
         System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());  
   
 driver.close();  
-    }  
+  
+  //In the above code, we haven't used the complete 'id' attribute value as it is very big. I have used the half part of the 'Senior Citizen' checkbox, and other half part is represented in the form of regular expression, i.e., '*='.
+  
+  //We have used two methods in the above code:
+  //isSelected(): This method determines whether the checkbox is selected or not. If the checkbox is selected, then this method returns true otherwise false.
+  //click(): This method selects the locator. In this case, it is selecting the "Senior Citizen" checkbox.
+}  
   
 }
